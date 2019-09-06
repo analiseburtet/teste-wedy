0bc0bb64f86ea076bbeb36f24bb8a9de8f6332f1
Analise Burtet <analiseburtet@gmail.com>
2019-09-06 19:26:41 -0300

    first layout adjustment

---
+++ src/components/textarea/textarea.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./textarea.styles.css";
+
+class TextArea extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { items: '' };
+      }
+      changeHandler = (event) => {
+        this.setState({items: event.target.value});
+      }
+      render() {
+        return (
+          <form>
+          <h1>To be distributed {this.state.items}</h1>
+          <p>Enter the items here:</p>
+            <textarea onChange={this.changeHandler}>
+            25kg geladeira x2
+            </textarea>
+          </form>
+        );
+      }
+    }
+
+export default TextArea;
